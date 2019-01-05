214c517cb7e4867fca95d1190b13c414e5e963db
deiva <deiva@192.168.2.112>
2019-01-05 09:57:14 +0100

    BehandlungsDAO Update

--- src/persistence/internal/BehandlungDAO.java
+++ src/persistence/internal/BehandlungDAO.java
@@ -4,6 +4,7 @@ import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.Set;
 
 import awk.entity.BehandlungTO;
 import persistence.IBehandlungDAO;
@@ -12,14 +13,24 @@ import persistence.IBehandlungDAO;
 public class BehandlungDAO implements IBehandlungDAO {
 	@Override
 	public void updateBehandlung(BehandlungTO behandlungTO) {
-		String begriff = behandlungTO.getBegriff();
-		String definition = behandlungTO.getDefinition();
+		int ID = behandlungTO.getBehandlungsID();
+		String datum = behandlungTO.getDatum();
+		Set<String> leistungen = behandlungTO.getLeistungen();
+		String arzt = behandlungTO.getArzt();
+		String patient = behandlungTO.getPatient();
+		String Behandlungsart = behandlungTO.getBehandlungsart()
 
 		Connection aConnection = Persistence.getConnection();
 
 		try {
 			Persistence.executeUpdateStatement(aConnection,
-					"INSERT INTO definitionen VALUES ('" + begriff + "','" + definition + "')");
+					"update behandlung set"
+					+ "arzt = '" + arzt + "', "
+					+ "patient = '" + patient + "', "
+					+ "datum = TO_DATE('2019-11-01', 'YYYY-MM-DD'), "
+					+ "behandlungsart = '" + Behandlungsart + "', "
+					+ "leistungen = '" + leistungen + "' "
+					+ "where ID = " + ID + "");
 
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
@@ -35,9 +46,11 @@ public class BehandlungDAO implements IBehandlungDAO {
 		ResultSet resultSet;
 		ArrayList<BehandlungTO> result = new ArrayList<BehandlungTO>();
 		try {
-			resultSet = Persistence.executeQueryStatement(aConnection, "SELECT * FROM definitionen");
+			resultSet = Persistence.executeQueryStatement(aConnection, "SELECT * FROM behandlungen");
 			while (resultSet.next()) {
-				result.add(new behandlungTO(resultSet.getString("begriff"), resultSet.getString("definition")));
+				result.add(new behandlungTO(resultSet.getString("id"), resultSet.getString("datum")
+						, resultSet.getString("leistungen"), resultSet.getString("arzt")
+						, resultSet.getString("patient"), resultSet.getString("behandlungsart")));
 			}
 
 		} catch (SQLException e) {
--- src/persistence/internal/Persistence.java
+++ src/persistence/internal/Persistence.java
@@ -1,5 +1,12 @@
 package persistence.internal;
 
+import java.sql.Connection;
+
 public class Persistence {
+
+	public static Connection getConnection() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
