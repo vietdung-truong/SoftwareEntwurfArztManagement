40be7392c07a531122c0089e641ef73b11909753
raphzahn1 <38244514+raphzahn1@users.noreply.github.com>
2019-01-09 14:04:07 +0100

    Behandlungscontroller

--- src/persistence/internal/Persistence.java
+++ src/persistence/internal/Persistence.java
@@ -28,7 +28,7 @@ public class Persistence {
 			/* 
 			 * Form: @Rechnername:Port, "Db-User", "Password"
 			 */
-			aConnection = DriverManager.getConnection ("jdbc:h2:~/softwareEntwurfTest", "as", "");
+			aConnection = DriverManager.getConnection ("jdbc:h2:~/softwareEntwurfTest", "sa", "");
 		} catch (SQLException e) {
 				e.printStackTrace();
 		}
